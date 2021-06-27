4c73e5915361f38384e6ed0373ba6230e5128de2
mergrey <66365439+gouri1310@users.noreply.github.com>
2021-06-27 14:43:42 +0530

    Add files via upload

---
+++ illumina_to_gene.R
@@ -0,0 +1,9 @@
+setwd("C:/GSE ID Scores")
+gse141484_df<-read.csv("C:/GSE ID Scores/GSE141484_non-normalized.csv")
+class(gse141484_df)
+probe_id=gse141484_df[['ID_REF']]
+print(probe_id)
+library("illuminaHumanv4.db") 
+gene_df=data.frame(Gene=unlist(mget(x = probe_id,envir = illuminaHumanv4SYMBOL)))
+print(gene_df)
+
